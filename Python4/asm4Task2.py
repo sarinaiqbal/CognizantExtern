ef97a29eddfa0001e7b61319deac209484df38fc
sarinaiqbal <166459050+sarinaiqbal@users.noreply.github.com>
2025-05-30 13:01:04 -0700

    Added comments

--- Python4/asm4Task2.py
+++ Python4/asm4Task2.py
@@ -1,18 +1,25 @@
+'''
+Author: Sarina Iqbal
+Python Assignment: Hands on Python Data Structures
+Task 2
+'''
+
+# Creating dictionary info
 info = {"name": "Sarina", "age":21, "city": "Tucson"}
 
-print(info)
-
+# Add a new key value pair
 info["favorite color"] = "blue"
 
+# Update 'city' key with new value
 info.update({"city":"Tampa"})
 
-print(info)
-
+# Printing keys
 print("Keys: ")
 for key in info:
-    print("-", key, end=" ")
+    print("-", key, end=" ") # all keys in same line with a preceding '-'
 print(" ")
 
+# Printing values
 print("Values:")
 for key, value in info.items():
-    print("-", value, end=" ")
+    print("-", value, end=" ") # all keys in same line with a preceding '-'
