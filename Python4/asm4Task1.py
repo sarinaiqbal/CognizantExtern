57a0ffb50b4e0fc1a4284f4721349f702f95bba7
sarinaiqbal <166459050+sarinaiqbal@users.noreply.github.com>
2025-05-30 12:37:16 -0700

    Added comments

--- Python4/asm4Task1.py
+++ Python4/asm4Task1.py
@@ -1,10 +1,19 @@
+'''
+Author: Sarina Iqbal
+Python Assignment: Hands on Python Data Structures
+Task 1
+'''
+# creating list
 fave_fruit = ["apple", "pear", "banana", "cherry", "mango"]
 print("Original list:", fave_fruit)
 
+# adding a fruit
 fave_fruit.append("watermelon")
 print("After adding a fruit:", fave_fruit)
 
+# removing a fruit
 fave_fruit.remove("pear")
 print("After removing a fruit:", fave_fruit)
 
+# reversing list
 print("Reversed list:", fave_fruit[::-1])
