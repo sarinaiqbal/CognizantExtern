c26857ef5f268f94b71bd2f584f961f9c616e5ef
sarinaiqbal <166459050+sarinaiqbal@users.noreply.github.com>
2025-06-04 02:21:31 +0600

    Added Python Project 2
    Project: Number Guessing Game

--- project2.py
+++ project2.py
@@ -1,41 +1,27 @@
 '''
 Author: Sarina Iqbal
-Python Project: Number
+Python Project: Number Guessing Game
 '''
 
 import random
 number_to_guess = random.randint(1,100)
-print(number_to_guess)
 
-user_guess = 0
-count = 0
+count = 0 # Keep track of number of user attempts
 
-'''while count<(11) :
-    while user_guess != number_to_guess:
-        user_guess = int(input("Guess a number between 1 and 100: "))
-        if user_guess > number_to_guess:
-            print("Too high! Try again.")
-
-        elif user_guess < number_to_guess:
-            print("Too low! Try again.")
-        count+=1
-    break
-print("Game over! Better luck next time!")
-
-print("Congratulations! You guessed it in", count, "attempts!")'''
-
-
-while count < 11:
+while count < 10: # Loop runs 10 times
     user_guess = int(input("Guess a number between 1 and 100: "))
-    if user_guess > number_to_guess:
+    count += 1 # Increment count
+    if user_guess > number_to_guess: # High guess
         print("Too high! Try again.")
-        count+=1
-    elif user_guess < number_to_guess:
+
+    elif user_guess < number_to_guess: # Low guess
         print("Too low! Try again.")
-        count += 1
-    else:
+
+    else: # Correct guess
         print("Congratulations! You guessed it in", count, "attempts!")
-        break
+        break # break out of loop when correct answer is guessed
+
+# If count == 10 and guess is still incorrect, end game with a message   
 if user_guess != number_to_guess:
     print("Game over! Better luck next time!")
 
